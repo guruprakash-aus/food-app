c1d657eda24c2ced6ac3a7a204a0de69edb6a6ae
guruprakash-aus <rajendranguruprakash@gmail.com>
2025-08-27 12:13:50 +1000

    Kafka-addition

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ start_db:
 
 stop_db:
 	docker compose -p food_app down
+
+start_kafka:
+	docker compose -f ./broker/docker-compose.yml -p food_app_kafka up -d
+
+stop_kafka:
+	docker compose -p food_app_kafka down
--- order_service/src/expressApp.ts
+++ order_service/src/expressApp.ts
@@ -3,21 +3,45 @@ import cors from "cors";
 
 import orderRoutes from "./routes/order.routes";
 import cartRoutes from "./routes/cart.routes";
-import { HandleErrorWithLogger, httpLogger } from "./utils";
+import { HandleErrorWithLogger, httpLogger, MessageBroker } from "./utils";
+import { Consumer, Producer } from "kafkajs";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.use(httpLogger);
+export const ExpressApp = async() => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  
+  app.use(httpLogger);
+  
 
-app.use(cartRoutes);
-app.use(orderRoutes);
+  //  Kafka Step 1: connect to producer and consumer
+  const producer = await MessageBroker.connectProducer<Producer>();
+  producer.on("producer.connect", () => {
+    console.log("producer connected")
+  })
 
-app.use("/", (req: Request, res: Response, _: NextFunction) => {
-  return res.status(200).json({ message: "Order Service is Healthy" });
-});
+  const consumer = await MessageBroker.connectConsumer<Consumer>();
+  consumer.on("consumer.connect", () => {
+    console.log("Consumer connected")
+  })
 
-app.use(HandleErrorWithLogger);
+  //  Kafka Step 2: Subscribe to topic or publish the message
+  await MessageBroker.subscribe((message) => {
+    // connect to catalog service
+    console.log("consumer received a message");
+    console.log("message received", message)
+  }, "OrderEvents")
 
-export default app;
+  app.use(cartRoutes);
+  app.use(orderRoutes);
+  
+  app.use("/", (req: Request, res: Response, _: NextFunction) => {
+    return res.status(200).json({ message: "Order Service is Healthy" });
+  });
+  
+  app.use(HandleErrorWithLogger);
+
+  return app
+}
+
